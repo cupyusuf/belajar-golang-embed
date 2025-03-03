1a67ee8251c88f08254d88498e11ecfedac9f464
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-03 17:03:40 +0700

    feat: embed di compile

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+)
+
+//go:embed version.txt
+var version string
+
+//go:embed sakura.jpeg
+var sakura []byte
+
+//go:embed files/*.txt
+var path embed.FS
+
+func main() {
+	fmt.Println(version)
+
+	err := ioutil.WriteFile("sakura_new.jpeg", sakura, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	dirEntries, _ := path.ReadDir("files")
+	for _, entry := range dirEntries {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			file, _ := path.ReadFile("files/" + entry.Name())
+			fmt.Println(string(file))
+		}
+	}
+}
