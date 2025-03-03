f3848fc92388acd736ca76d3fdd0c8cbeaf233a6
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-03 16:35:11 +0700

    feat: add embed []byte

--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package belajar_golang_embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -11,4 +13,17 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed sakura.jpeg
+
+var sakura []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("sakura_copy.jpeg", sakura, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("success copy file")
 }
