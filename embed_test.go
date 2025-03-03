e079fbce48185e856f763734a48f68c16da65774
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-03 16:40:20 +0700

    feat: add embed multiple file

--- embed_test.go
+++ embed_test.go
@@ -1,6 +1,7 @@
 package belajar_golang_embed
 
 import (
+	"embed"
 	_ "embed"
 	"fmt"
 	"io/fs"
@@ -26,4 +27,20 @@ func TestByte(t *testing.T) {
 	}
 
 	fmt.Println("success copy file")
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+
+var files embed.FS
+
+func TestMultipleFiles(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	b, _ := files.ReadFile("files/b.txt")
+	c, _ := files.ReadFile("files/c.txt")
+
+	fmt.Println(string(a))
+	fmt.Println(string(b))
+	fmt.Println(string(c))
 }
